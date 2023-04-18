72114707eefc7315ce274c7f4d34030ce3aad9eb
felipepperez <fpproducoes@gmail.com>
2023-04-17 21:13:23 -0400

    Aula 17/04/2023

---
+++ 20230417/routes/search.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+const json = JSON.parse(fs.readFileSync('./food.json'));
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('search', { json });
+});
+
+router.get('/:id', function (req, res, next) {
+  const { id } = req.params;
+  const elemento = json.categories.find((elemento) => id == elemento.idCategory);
+  if (elemento) {
+    res.render('comidas', { json: { categories: [elemento] } });
+  } else {
+    res.render('error', { message: 'Not Found', error: { status: 404 } });
+  }
+});
+
+module.exports = router;
